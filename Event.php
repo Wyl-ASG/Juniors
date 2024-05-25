e2338dcce0e54f196c3d96e857c711ff0874c09e
Kritic8 <kyleliow888@gmail.com>
2024-05-26 00:40:41 +0800

    event class

--- Event.php
+++ Event.php
@@ -8,5 +8,22 @@ class Event{
     private $date;
     private $start;
     private $end;
+
+    private $capacity;
+
+    private $description;
+
+    public function __construct($event_id,$event_name,$username,
+    $location, $date,$start,$end, $capacity,$description){
+        $this->event_id= $event_id;
+        $this->username = $username;
+        $this->location = $location;
+        $this->date = $date;
+        $this->start = $start;
+        $this->end = $end;
+        $this->capacity = $capacity;
+        $this->description = $description;
+    }
     
 }
+
